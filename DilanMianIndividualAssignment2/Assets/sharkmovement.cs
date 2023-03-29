4c544066b214f8cf678309da01bba8faa5ba9a10
miandilan <miandilan@gmail.com>
2023-03-29 12:49:14 -0400

    shark movement update

--- DilanMianIndividualAssignment2/Assets/sharkmovement.cs
+++ DilanMianIndividualAssignment2/Assets/sharkmovement.cs
@@ -11,26 +11,24 @@ public class sharkmovement : MonoBehaviour
 
     void Start()
     {
-        //get the normal of the plane object
+        //water normal
         planeNorm = plane.transform.up;
     }
 
     void Update()
     {
-        //get the horizontal and vertical axis input
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        //calculate the movement direction based on the input
         Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput);
 
-        //if there is input, move the object
+        //if the move buttons are pushed (wasd or the arrow keys), move the shark
         if (movementDirection != Vector3.zero)
         {
-            //clamp the movement direction to the plane
+            //clamp the movement direction to the water
             movementDirection = Vector3.ProjectOnPlane(movementDirection, planeNorm).normalized;
 
-            //move the object
+            //move the shark
             transform.position += movementDirection * speed * Time.deltaTime;
         }
     }
